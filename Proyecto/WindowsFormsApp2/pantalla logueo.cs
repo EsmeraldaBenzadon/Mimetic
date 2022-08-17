d71c98406d9cc08e69e84ebb65ed0f8fe73d5bcf
EsmeraldaBenzadon <110538279+EsmeraldaBenzadon@users.noreply.github.com>
2022-08-17 11:37:31 -0300

    Proyecto Visual

---
+++ Proyecto/WindowsFormsApp2/pantalla logueo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class pantalla_logueo : Form
+    {
+        public pantalla_logueo()
+        {
+            InitializeComponent();
+        }
+
+        private void Pantalla_logueo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
